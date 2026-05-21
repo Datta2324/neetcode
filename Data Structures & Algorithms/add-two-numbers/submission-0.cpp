344aae25e1fb5cb37cb7161e5b4e33829c7ad36e
Manasa Datta <78838325+Datta2324@users.noreply.github.com>
2026-05-21 14:45:27 +0530

    Add: add-two-numbers - submission-0

---
+++ Data Structures & Algorithms/add-two-numbers/submission-0.cpp
@@ -0,0 +1,87 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+    if(l1 == nullptr)
+        return l2;
+    if(l2 == nullptr)
+        return l1;
+
+        int carry = 0;
+        int sum ;
+        ListNode *one = l1;
+        ListNode *two = l2;
+        ListNode *last, *lastTwo;
+        while(one != nullptr && two != nullptr)
+        {
+            sum = one->val + two->val + carry;
+            cout<< one-> val <<" + "<< two->val<< " ="<<sum<<" \n";
+
+            int digit = sum % 10;
+            carry = sum /10;
+
+            one->val = digit;
+            two->val = digit;
+            last = one;
+            lastTwo = two;
+            one = one->next;
+            two = two->next;
+        }
+        if(one)
+        {
+            lastTwo = nullptr;
+            while(one)
+            {
+                sum = one->val + carry;
+
+                int digit = sum % 10;
+                carry = sum /10;
+
+                one->val = digit;
+                last = one;
+                one = one->next;
+            }
+
+        }
+        if(two)
+        {
+            last = nullptr;
+            while(two)
+            {
+                sum = two->val + carry;
+
+                int digit = sum % 10;
+                carry = sum /10;
+
+                two->val = digit;
+                lastTwo = two;
+                two = two->next;
+            }
+        }
+        if(carry)
+        {
+            ListNode* insert = new ListNode(carry);
+            if(last)
+                last->next = insert;
+            else
+                lastTwo->next = insert;
+
+        }
+        if(last)
+            return l1;
+        else
+            return l2;
+        
+    }
+};
