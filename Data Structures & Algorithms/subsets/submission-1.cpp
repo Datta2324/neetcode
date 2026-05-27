a5aaaf1c68e19dcc3f8434e6e2b1c5778ec774a8
Manasa Datta <78838325+Datta2324@users.noreply.github.com>
2026-05-27 09:41:44 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    // Final answer storage
+    vector<vector<int>> ans;
+    // Current subset being built
+    vector<int> temp;
+
+    // Recursive DFS function
+    void dfs(int i, vector<int>& nums)
+    {
+        // BASE CASE
+        // We processed all elements
+        // Current temp is one complete subset
+        if(i == nums.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+
+        // TAKE CURRENT ELEMENT
+        // Choose nums[i]
+        temp.push_back(nums[i]);
+
+        // Move to next index
+        dfs(i + 1, nums);
+
+        // BACKTRACK
+        // Undo the previous choice
+        // so skip branch starts clean
+        temp.pop_back();
+
+        // NOT TAKE CURRENT ELEMENT
+        // Skip nums[i]
+        dfs(i + 1, nums);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        // Start recursion from index 0
+        dfs(0, nums);
+
+        return ans;
+    }
+};
