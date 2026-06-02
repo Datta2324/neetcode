7d58c73e218c5cdb37fd53fa3d99b0fc3205ac86
Manasa Datta <78838325+Datta2324@users.noreply.github.com>
2026-06-02 09:35:25 +0530

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.cpp
@@ -0,0 +1,30 @@
+#include <string>
+class Solution {
+public:
+    vector<string> ans;
+    string temp;
+    void dfs(int pos, string &digits, vector<string> &vec)
+    {
+        if(temp.size() == digits.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i = 0; i < vec[(digits[pos] - '0')].size(); i++)
+        {
+            temp.push_back(vec[digits[pos] - '0'][i]);
+            dfs(pos+1,digits,vec);
+            temp.pop_back();
+        }
+
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())
+            return {};
+        vector<string> vec = {"Jiraya","sensei","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        dfs(0,digits,vec);
+        return ans;
+        
+    }
+};
