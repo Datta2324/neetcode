f29de3d61edb68b594a5231f3ebc70ebbcf5d9ef
Manasa Datta <78838325+Datta2324@users.noreply.github.com>
2026-06-17 16:33:38 +0530

    Add: path-sum - submission-0

---
+++ Data Structures & Algorithms/path-sum/submission-0.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr)
+           return false;
+        if(root->left == nullptr && root->right == nullptr)
+        {
+            if(targetSum - root->val == 0)
+                return true;
+            else
+                return false;
+        }
+
+        int left = hasPathSum(root->left,targetSum - root->val);
+        if(left == true)
+            return true;
+        int right = hasPathSum(root->right,targetSum - root->val);
+
+        return (left || right); 
+    }
+};
