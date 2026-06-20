88363ec8e1d9827e6c447820f45b64441ff460a1
Manasa Datta <78838325+Datta2324@users.noreply.github.com>
2026-06-20 13:54:19 +0530

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l = 0, m = 0,r = nums.size() -1 ;
+
+        while(m <= r)
+        {
+            if(nums[m] == 1)
+                m++;
+            else if(nums[m] == 0)
+            {
+                swap(nums[l],nums[m]);
+                m++;
+                l++;
+            }
+            else
+            {
+                swap(nums[r],nums[m]);
+                r--;
+            }
+        }
+        
+    }
+};
