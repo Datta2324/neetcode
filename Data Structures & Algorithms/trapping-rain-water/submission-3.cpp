28be455b2e22646227aec7025faeb2a684673a85
Manasa Datta <78838325+Datta2324@users.noreply.github.com>
2026-06-22 19:22:30 +0530

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.cpp
@@ -0,0 +1,43 @@
+class Solution {
+   public:
+    int rightMax(int i , vector<int>& height, int n)
+    {
+        int updated = -1;
+        int rm =  i;
+        
+        while(i < n)
+        {
+            if(height[i] >=height[rm])
+            {
+                rm = i;
+                updated = i;
+            }
+            i++;
+        }
+
+        return updated;
+
+    }
+    
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l = 0;
+        while (l < n && height[l] == 0) l++;
+
+        int lm = l;
+        int rm = l - 1;
+        int count = 0;
+
+        for (int i = l; i < n; i++) {
+            // Find right max
+            if (rm <= i) rm = rightMax(i + 1, height, n);
+
+            if (rm == -1) continue;
+
+            count += max(0, (min(height[lm], height[rm]) - height[i]));
+            if (height[i] > height[lm]) lm = i;
+        }
+
+        return count;
+    }
+};
