1d48a6352121b583190df06dff85ae5e5c722083
Manasa Datta <78838325+Datta2324@users.noreply.github.com>
2026-07-01 07:41:10 +0530

    Add: zigzag-conversion - submission-1

---
+++ Data Structures & Algorithms/zigzag-conversion/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int n = s.size();
+        vector<string> ans(numRows);
+        int i = 0;
+        if (numRows == 1)
+            return s;
+        while(n)
+        {
+            int p = 0;
+            while( n && (p < numRows))
+            {
+                ans[p]+= s[i];
+                i++;
+                p++;
+                n--;
+            }
+            p = numRows - 2;
+            while(n && p)
+            {
+                ans[p] += s[i];
+                i++;
+                p--;
+                n--;
+            }
+        }
+        for(int i = 1; i < numRows;i++)
+        {
+            ans[0]+=ans[i];
+        }
+
+        return ans[0];
+    }
+};
