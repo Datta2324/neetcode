e30152e6fc0d60fff78cbb5347c39871e373869c
Manasa Datta <78838325+Datta2324@users.noreply.github.com>
2026-07-07 14:53:32 +0530

    Add: search-for-word - submission-6

---
+++ Data Structures & Algorithms/search-for-word/submission-6.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int dirs[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+    bool dfs(int m, int n, int i, int j, int pos, string &word,vector<vector<char>>& board)
+    {
+        if(word[pos] != board[i][j])
+            return false;
+            
+        if(pos == word.size() - 1)
+            return true;
+    
+        bool ans = false;
+        for(auto x : dirs)
+        {
+            int nx = x[0] + i;
+            int ny = x[1] + j;
+
+            if((nx >= 0) && (nx < m) && (ny >=0) && (ny < n) && (board[nx][ny] != '#'))
+            {
+                char temp = board[i][j];
+                board[i][j] = '#';
+                ans = dfs(m,n,nx,ny,pos+1,word,board);
+                board[i][j] = temp;
+                if(ans)
+                    return true;
+            }
+        }
+        return ans;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+
+        for(int i = 0 ; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(board[i][j] == word[0])
+                {
+                    bool ans = dfs(m,n,i,j,0,word,board);
+                    if(ans)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+};
